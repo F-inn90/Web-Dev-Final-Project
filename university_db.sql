7c105836ccda1a5cb5965d622a3ccde8286e341a
hengvuth80 <74618633+hengvuth80@users.noreply.github.com>
2025-08-08 18:09:18 +0700

    Add files via upload

---
+++ university_db.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 08, 2025 at 01:08 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `university_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news_feed`
+--
+
+CREATE TABLE `news_feed` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `news_feed`
+--
+
+INSERT INTO `news_feed` (`id`, `title`, `description`, `created_at`) VALUES
+(1, 'University Reopens Fall Semester', 'The university will reopen the fall semester on September 1st with new health protocols.', '2025-08-08 10:46:12'),
+(2, 'New Computer Lab Available', 'A new state-of-the-art computer lab has been opened in the Science Building for student use.', '2025-08-08 10:46:12'),
+(3, 'Scholarship Application Deadline', 'Reminder: The deadline to apply for scholarships for the 2025 academic year is August 15th.', '2025-08-08 10:46:12'),
+(4, 'Guest Lecture Series', 'A series of guest lectures on Artificial Intelligence will be held every Friday this semester.', '2025-08-08 10:46:12'),
+(5, 'Campus Wi-Fi Upgrade', 'Campus-wide Wi-Fi is being upgraded to improve speed and reliability for all students and staff.', '2025-08-08 10:46:12'),
+(6, 'penis', 'hi', '2025-08-08 06:05:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstudents`
+--
+
+CREATE TABLE `tblstudents` (
+  `id` int(11) NOT NULL,
+  `idcard` varchar(50) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `class` varchar(50) NOT NULL,
+  `ranks` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tblstudents`
+--
+
+INSERT INTO `tblstudents` (`id`, `idcard`, `firstname`, `lastname`, `gender`, `address`, `class`, `ranks`) VALUES
+(1, '123', 'hi', 'hi', 'Male', '1231', '123123', 1),
+(2, '69420', 'first name', 'hi', 'Male', 'ur mom st', '1', 222);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `news_feed`
+--
+ALTER TABLE `news_feed`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `news_feed`
+--
+ALTER TABLE `news_feed`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
